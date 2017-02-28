c2262a38bf7e94a19f833ccb89c2653ddaaa7fcf
Lukas Erlacher <erlacher@in.tum.de>
2017-02-28 09:16:23 +0100

    Show maintenance banner as message if in session

--- maintenance.php
+++ maintenance.php
@@ -24,7 +24,6 @@ class maintenance extends rcube_plugin
 		$this->maint_start = $this->rc->config->get('maintenance_start');
 		$this->maint_end = $this->rc->config->get('maintenance_end');
 		$maint_pre = $this->rc->config->get('maintenance_pre');
-
 		// calculate timeframe
 		$now = time();
 
@@ -35,14 +34,27 @@ class maintenance extends rcube_plugin
 
 		if ($this->is_maint || $this->upcoming_maint)
 		{
-			$this->include_script('maintenance.js');
-			$this->include_stylesheet($this->local_skin_path() .'/maintenance.css');
-			$this->add_hook('template_object_message', array($this, 'addMaintBanner'));
 			$this->add_texts('localization/', true);
+
+			if ($this->rc->task == 'login' || $this->rc->task == 'logout')
+			{
+				$this->include_script('maintenance.js');
+				$this->include_stylesheet($this->local_skin_path() .'/maintenance.css');
+				$this->add_hook('template_object_message', array($this, 'addMaintBanner'));
+			}
+			else
+			{
+				if (!isset($_SESSION['maintenance_banner_seen']) || !$_SESSION['maintenance_banner_seen'])
+				{
+					$this->rc->output->show_message($this->getMaintBannerText());
+					$_SESSION['maintenance_banner_seen'] = true;
+				}
+			}
 		}
 	}
 
-	function addMaintBanner($p) {
+	function getMaintBannerText()
+	{
 		$text = array(
 			'name' => ($this->is_maint) ? 'maintenanceBanner' : 'preMaintenanceBanner',
 			'vars' => array(
@@ -50,7 +62,12 @@ class maintenance extends rcube_plugin
 				'end' => $this->rc->format_date($this->maint_end)
 			),
 		);
-		$p['content'] = '<div id="maintbanner">'.$this->gettext($text).'</div>'.$p['content'];
+		return $this->gettext($text);
+	}
+
+	function addMaintBanner($p) {
+		$text = $this->getMaintBannerText();
+		$p['content'] = '<div id="maintbanner">'.$text.'</div>'.$p['content'];
 		return $p;
 	}
 }
